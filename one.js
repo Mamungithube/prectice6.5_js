edc66be89a2a89dab7bcb3cb6b0d4b569f9c031a
mamungithube <orroshidmdmamun50@gmail.com>
2024-05-26 21:43:42 +0600

    massage

--- one.js
+++ one.js
@@ -1,1 +1,19 @@
-console.log("object");
+const number = 36;
+if(number >= 80){
+    console.log(" You got A+");
+}
+else if(number >= 70 && number <= 79){
+    console.log(" You got A");
+}
+else if(number >= 60 && number <= 69){
+    console.log(" You got A-");
+}
+else if(number >= 50 && number <= 59){
+    console.log(" You got B");
+}
+else if(number >= 40 && number <= 49){
+    console.log(" You got c");
+}
+else{
+    console.log("You got F");
+}
